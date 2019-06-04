7790a26d92881da819574231cff178081dc238cf
basaamp <44131166+basaamp@users.noreply.github.com>
2019-06-05 00:02:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+app.get('/process_get', function (req, res) {
 
-
+   // Prepare output in JSON format
+   response = {
+       first_name:req.query.first_name,
+       last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+/*
 app.get('/', function (req, res) {
    res.send('ujjii');
 })
-
+*/
   /*.get('/', (req, res) => res.render('pages/index'))*/
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
