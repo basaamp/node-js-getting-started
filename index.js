dcd45f59fe2cab9cc4c710b8752277e5a0207115
basaamp <44131166+basaamp@users.noreply.github.com>
2019-06-04 22:53:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
+var app = express();
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+  /*.get('/', (req, res) => res.render('pages/index'))*/
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
