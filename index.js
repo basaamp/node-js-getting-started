e12e6844cc3ed477638254fc40f0015d8f0e2184
basaamp <44131166+basaamp@users.noreply.github.com>
2019-06-12 20:16:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 5000
 
 var app = express()
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // create application/json parser
 var jsonParser = bodyParser.json()
 
