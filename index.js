922d4c33e4cfe7bac91b27137633f1bc522946d4
basaamp <44131166+basaamp@users.noreply.github.com>
2019-06-12 21:07:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // POST /login gets urlencoded bodies
 app.post('/login', urlencodedParser, function (req, res) {
-  res.send('welcome, ' + req.body.username)
+  res.send('welcome, ' + JSON.stringify(req.body.username))
 })
 
 // POST /api/users gets JSON bodies
